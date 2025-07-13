66992abb95aaa23199f48dca4fb8394a68eac194
MUKUL6121 <148455498+MUKUL6121@users.noreply.github.com>
2025-07-13 19:53:26 +0530

    Update Express.js

--- Express.js
+++ Express.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path')
 const app = express();
 const PORT = 5500;
-const a = '../../HTML CSS JS course/Assignement/KFC.html'
+// const a = '../../HTML CSS JS course/Assignement/KFC.html'
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, a))
+    // res.sendFile(path.join(__dirname, a))
+    res.send("Welcome, to new file");
 });
 app.post('/post', (req, res) => {
     res.send(`<h1>hello</h1>\nData Posted.`);
@@ -19,5 +20,6 @@ app.delete('/delete', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+    // console.log(`http://localhost:${PORT})
+    conseol.log("server___Started");
 })
