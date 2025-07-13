a7ebf98f0de00de76a9ba0b4736b17e0a36e7ce1
MUKUL6121 <148455498+MUKUL6121@users.noreply.github.com>
2025-07-13 19:44:28 +0530

    Add files via upload

---
+++ Express.js
@@ -0,0 +1,23 @@
+//thsi file contain express.js code that how to use it.
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 5500;
+const a = '../../HTML CSS JS course/Assignement/KFC.html'
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, a))
+});
+app.post('/post', (req, res) => {
+    res.send(`<h1>hello</h1>\nData Posted.`);
+});
+app.put('/put', (req, res) => {
+    res.send(`<h1>hello</h1>\nData Updated.`);
+});
+app.delete('/delete', (req, res) => {
+    res.send(`<h1>hello</h1>\nData Deleted.`);
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
